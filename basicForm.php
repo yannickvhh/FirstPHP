44fc1b4c8cb77d376827bf6cf3e562d0300b2e46
Yannick Van Heerden <134289088+yannickvhh@users.noreply.github.com>
2025-05-05 23:55:16 +0200

    Update basicForm.php

--- basicForm.php
+++ basicForm.php
@@ -1,11 +1,25 @@
 <html>
 <head>
 <title>A BASIC HTML FORM</title>
+<?PHP
+$username = $_POST['username'];
+if ($username == "letmein") {
+print ("Welcome back, friend!");
+}
+else {
+print ("You're not a member of this site");
+}
+
+?>
+
 </head>
 <body>
-<FORM NAME ="form1" METHOD =" " ACTION = "">
-<INPUT TYPE = "TEXT" VALUE ="username">
+<FORM NAME ="form1" METHOD ="POST" ACTION = "basicForm.php">
+<INPUT TYPE = "TEXT" VALUE ="username" NAME="username">
 <INPUT TYPE = "Submit" Name = "Submit1" VALUE = "Login">
+
 </FORM>
+
+
 </body>
 </html>
